75b63825274f01f1f063ccfe4ec02b012636fa2d
Jinho Hong <tpdleps@gmail.com>
2018-08-29 20:43:20 +0900

    prettier , firehouse 추가

--- src/handler.js
+++ src/handler.js
@@ -1,17 +1,32 @@
-'use strict';
+"use strict";
 
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
+const AWS = require("aws-sdk");
 
 exports.collect = (event, context, callback) => {
+  const body = JSON.parse(event.body);
+  const firehose = new AWS.Firehose();
+  console.log(`Body payload: ${JSON.stringify(body, null, 2)}`);
+
+  firehose
+    .putRecord({
+      DeliveryStreamName: "DataTracker-prod",
+      Record: {
+        Data: `${JSON.stringify(body)}\n`
+      }
+    })
+    .promise()
+    .then(res => res)
+    .catch(err => console.error(err));
   callback(null, {
     statusCode: 200,
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json"
     },
     body: JSON.stringify({
-      data: { success: true },
-    }),
+      data: { success: true }
+    })
   });
 };
 
@@ -19,8 +34,10 @@ exports.index = (event, context, callback) => {
   callback(null, {
     statusCode: 200,
     headers: {
-      'Content-Type': 'text/html; charset=UTF-8',
+      "Content-Type": "text/html; charset=UTF-8"
     },
-    body: fs.readFileSync(path.join(__dirname, 'index.html'), { encoding: 'utf8' }),
+    body: fs.readFileSync(path.join(__dirname, "index.html"), {
+      encoding: "utf8"
+    })
   });
 };
